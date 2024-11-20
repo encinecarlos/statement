c0b7a0a4307ff708075a3273230fbc4babe1a3c9
Carlos Encine <carlos.encine@outlook.com>
2024-11-20 15:11:48 -0300

    feat: move calculations to class

--- generateStatementData.js
+++ generateStatementData.js
@@ -9,10 +9,12 @@ function generateStatementData(invoice, plays) {
   return statementData;
 
   function enrichPerformance(performance) {
+    const calulator = new PerformanceCalculator(performance, playFor(performance));
+
     const result = Object.assign({}, performance);
-    result.play = playFor(result);
-    result.amount = amountFor(result);
-    result.volumeCredits = volumeCreditsFor(result);
+    result.play = calulator.play;
+    result.amount = calulator.amount;
+    result.volumeCredits = calulator.volumeCredits;
     return result;
   }
 
@@ -21,37 +23,11 @@ function generateStatementData(invoice, plays) {
   }
 
   function amountFor(performance) {
-    let result = 0;
-    switch (performance.play.type) {
-      case "tragedy":
-        result = 40000;
-        if (performance.audience > 30) {
-          result += 1000 * (performance.audience - 30);
-        }
-        break;
-      case "comedy":
-        result = 30000;
-        if (performance.audience > 20) {
-          result += 10000 + 500 * (performance.audience - 20);
-        }
-        result += 300 * performance.audience;
-        break;
-      default:
-        throw new Error(`unknown type: ${performance.play.type}`);
-    }
-
-    return result;
+    return new PerformanceCalculator(performance, playFor(performance)).amount;
   }
 
   function volumeCreditsFor(performance) {
-    let result = 0;
-
-    result += Math.max(performance.audience - 30, 0);
-
-    if ("comedy" === performance.play.type)
-      result += Math.floor(performance.audience / 5);
-
-    return result;
+    
   }
 
   function calculateTotalAmount(data) {
@@ -62,4 +38,47 @@ function generateStatementData(invoice, plays) {
     return data.performances.reduce((total, perf) => total + perf.volumeCredits, 0);
   }
 }
+
+class PerformanceCalculator {
+  constructor(performance, play) {
+    this.performance = performance;
+    this.play = play;
+  }
+
+  get amount() {
+    let result = 0;
+    switch (this.play.type) {
+      case "tragedy":
+        result = 40000;
+        if (this.performance.audience > 30) {
+          result += 1000 * (this.performance.audience - 30);
+        }
+        break;
+      case "comedy":
+        result = 30000;
+        if (this.performance.audience > 20) {
+          result += 10000 + 500 * (this.performance.audience - 20);
+        }
+        result += 300 * this.performance.audience;
+        break;
+      default:
+        throw new Error(`unknown type: ${this.play.type}`);
+    }
+
+    return result;
+  }
+
+  get volumeCredits() {
+    let result = 0;
+
+    result += Math.max(this.performance.audience - 30, 0);
+
+    if ("comedy" === this.play.type)
+      result += Math.floor(this.performance.audience / 5);
+
+    return result;
+  }
+   
+}
+
 exports.generateStatementData = generateStatementData;

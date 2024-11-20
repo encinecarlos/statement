0ab3e91e673e51b651e62b584d877c5c1e732ba5
Carlos Encine <carlos.encine@outlook.com>
2024-11-20 13:14:55 -0300

    feat: create hml render and separate statement data generation

--- statement.js
+++ statement.js
@@ -1,74 +1,28 @@
+const { generateStatementData } = require("./generateStatementData");
+
 function statement(invoice, plays) {
-  const statementData = {};
+  return RenderPlainText(generateStatementData(invoice, plays));
+}
 
-  statementData.customer = invoice.customer;
-  statementData.performances = invoice.performances.map(enrichPerformance);
-  statementData.totalAmount = calculateTotalAmount(statementData);
-  statementData.totalVolumeCredits = totalVolumeCredits(statementData);
+function htmlstatement(invoice, plays) {
+  return renderHtml(generateStatementData(invoice, plays));
+}
 
-  return RenderPlainText(statementData);
-
-  function enrichPerformance(performance) {
-    const result = Object.assign({}, performance);
-    result.play = playFor(result);
-    result.amount = amountFor(result);
-    result.volumeCredits = volumeCreditsFor(result);
-    return result;
+function renderHtml(data) {
+  let result = `<h1>Statement for ${data.customer}</h1>\n`;
+  
+  result += "<table>\n";
+  result += "<tr>th>Play</th><yh>Seats</th><th>Cost</th></tr>";
+  
+  for (let perf of data.performances) {
+    result += ` <tr><td>${perf.play.name}</td><td>${perf.audience}</td>`;
+    result += `<td>${usd(perf.amount)}</td></tr>\n`;
   }
 
-  function playFor(performance) {
-    return plays[performance.playID];
-  }
-
-  function amountFor(performance) {
-    let result = 0;
-    switch (performance.play.type) {
-      case "tragedy":
-        result = 40000;
-        if (performance.audience > 30) {
-          result += 1000 * (performance.audience - 30);
-        }
-        break;
-      case "comedy":
-        result = 30000;
-        if (performance.audience > 20) {
-          result += 10000 + 500 * (performance.audience - 20);
-        }
-        result += 300 * performance.audience;
-        break;
-      default:
-        throw new Error(`unknown type: ${performance.play.type}`);
-    }
-
-    return result;
-  }
-
-  function volumeCreditsFor(performance) {
-    let result = 0;
-
-    result += Math.max(performance.audience - 30, 0);
-
-    if ("comedy" === performance.play.type)
-      result += Math.floor(performance.audience / 5);
-
-    return result;
-  }
-
-  function calculateTotalAmount(data) {
-    // let result = 0;
-    // for (let perf of data.performances) {
-    //   result += perf.amount;
-    // }
-    return data.performances.reduce((total, perf) => total + perf.amount, 0);
-  }
-
-  function totalVolumeCredits(data) {
-    // let result = 0;
-    // for (let perf of data.performances) {
-    //   result += perf.volumeCredits;
-    // }
-    return data.performances.reduce((total, perf) => total + perf.volumeCredits, 0);
-  }
+  result += "</table>\n";
+  result += `<p>Amount owned is <em>${usd(data.totalAmount)}</em> credits</p>\n`;
+  
+  return result; 
 }
 
 
@@ -82,15 +36,14 @@ function RenderPlainText(data) {
   result += `Amount owed is ${usd(data.totalAmount)}\n`;
   result += `You earned ${data.totalVolumeCredits} credits\n`;
   return result;
-  
-        
-  function usd(aNumber) {
-      return new Intl.NumberFormat("en-US", {
-        style: "currency",
-        currency: "USD",
-        minimumFractionDigits: 2,
-    }).format(aNumber / 100);
-  }
+}
+
+function usd(aNumber) {
+  return new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+    minimumFractionDigits: 2,
+  }).format(aNumber / 100);
 }
 
 

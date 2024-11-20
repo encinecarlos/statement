1d0c6aa435f695bd68a486b33ddc6e964e3977f7
Carlos Encine <carlos.encine@outlook.com>
2024-11-20 12:42:07 -0300

    feat: move amount calculation function

--- statement.js
+++ statement.js
@@ -9,11 +9,35 @@ function statement(invoice, plays) {
   function enrichPerformance(performance) {
     const result = Object.assign({}, performance);
     result.play = playFor(result);
+    result.amount = amountFor(result);
     return result;
   }
 
   function playFor(performance) {
     return plays[performance.playID];
+  }
+
+  function amountFor(performance) {
+    let result = 0;
+    switch (performance.play.type) {
+      case "tragedy":
+        result = 40000;
+        if (performance.audience > 30) {
+          result += 1000 * (performance.audience - 30);
+        }
+        break;
+      case "comedy":
+        result = 30000;
+        if (performance.audience > 20) {
+          result += 10000 + 500 * (performance.audience - 20);
+        }
+        result += 300 * performance.audience;
+        break;
+      default:
+        throw new Error(`unknown type: ${performance.play.type}`);
+    }
+
+    return result;
   }
 }
 
@@ -22,7 +46,7 @@ function RenderPlainText(data, plays) {
   let result = `Statement for ${data.customer}\n`;
   
   for (let perf of data.performances) {
-    result += ` ${perf.play.name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`;
+    result += ` ${perf.play.name}: ${usd(perf.amount)} (${perf.audience} seats)\n`;
   }
   
   result += `Amount owed is ${usd(calculateTotalAmount())}\n`;
@@ -32,7 +56,7 @@ function RenderPlainText(data, plays) {
   function calculateTotalAmount() {
     let result = 0;
     for (let perf of data.performances) {
-      result += amountFor(perf);
+      result += perf.amount;
     }
     return result;
   }
@@ -45,28 +69,7 @@ function RenderPlainText(data, plays) {
     return result;
   }
   
-  function amountFor(performance) {
-    let result = 0;
-    switch (performance.play.type) {
-      case "tragedy":
-        result = 40000;
-        if (performance.audience > 30) {
-          result += 1000 * (performance.audience - 30);
-        }
-        break;
-        case "comedy":
-          result = 30000;
-          if (performance.audience > 20) {
-            result += 10000 + 500 * (performance.audience - 20);
-          }
-          result += 300 * performance.audience;
-          break;
-          default:
-            throw new Error(`unknown type: ${performance.play.type}`);
-          }
-          
-          return result;
-        }
+  
         
         
         function volumeCreditsFor(performance) {

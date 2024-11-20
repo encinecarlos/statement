cc70517b31cbc1219481d82d5bae04425fe9f1a1
Carlos Encine <carlos.encine@outlook.com>
2024-11-20 12:15:34 -0300

    feat: extract total amount calculation

--- statement.js
+++ statement.js
@@ -1,27 +1,31 @@
 function statement(invoice, plays) {
-  let totalAmount = 0;
   let result = `Statement for ${invoice.customer}\n`;
-
+  
   for (let perf of invoice.performances) {
     result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${
       perf.audience
     } seats)\n`;
-    totalAmount += amountFor(perf);
   }
-
-  let volumeCredits = totalVolumeCredits();
-
-  result += `Amount owed is ${usd(totalAmount)}\n`;
-  result += `You earned ${volumeCredits} credits\n`;
+  
+  result += `Amount owed is ${usd(calculateTotalAmount())}\n`;
+  result += `You earned ${totalVolumeCredits()} credits\n`;
   return result;
 
-    function totalVolumeCredits() {
-        let volumeCredits = 0;
-        for (let perf of invoice.performances) {
-            volumeCredits += volumeCreditsFor(perf);
-        }
-        return volumeCredits;
+  function calculateTotalAmount() {
+    let result = 0;
+    for (let perf of invoice.performances) {
+      result += amountFor(perf);
     }
+    return result;
+  }
+
+  function totalVolumeCredits() {
+      let result = 0;
+      for (let perf of invoice.performances) {
+          result += volumeCreditsFor(perf);
+      }
+      return result;
+  }
 
   function amountFor(performance) {
     let result = 0;
